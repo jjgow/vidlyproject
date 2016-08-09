d52d4b52c644766af5228805a7b5d31732cf21fc
jjgow <jasingow@gmail.com>
2016-08-09 23:22:45 +1200

    alter userroles for adding/updating customers

--- Vidly/Controllers/CustomersController.cs
+++ Vidly/Controllers/CustomersController.cs
@@ -66,7 +66,10 @@ namespace Vidly.Controllers
 
         public ViewResult Index()
         {
-            return View();
+            if (User.IsInRole(RoleName.CanManageCustomers))
+                return View("List");
+
+            return View("ReadOnlyList");
         }
 
         public ActionResult Details(int id)
